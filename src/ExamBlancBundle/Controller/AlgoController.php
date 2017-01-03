f805dfabd1613c9fa5bc19a340775f69b860984a
Jasdero <houssemaine.j@gmail.com>
2017-01-03 14:50:55 +0100

    1er algo ok

--- src/ExamBlancBundle/Controller/AlgoController.php
+++ src/ExamBlancBundle/Controller/AlgoController.php
@@ -13,7 +13,17 @@ class AlgoController extends Controller
     // Exercice 1
     public function number_of_char($str)
     {
-
+        $str = strtolower($str);
+        $tab = str_split($str);
+        $tabletters = [];
+        foreach ($tab as $char) {
+            if (ctype_alnum($char) === true) {
+                $tabletters[] = $char;
+            }
+        }
+        sort($tabletters);
+        $result = array_count_values($tabletters);
+        return $result;
 
     }
 
