df87f2292e7ec72c63c93317eb5bae9c768e8942
Jasdero <houssemaine.j@gmail.com>
2017-01-03 15:44:47 +0100

    pause sur algo 2

--- src/ExamBlancBundle/Controller/AlgoController.php
+++ src/ExamBlancBundle/Controller/AlgoController.php
@@ -30,7 +30,47 @@ class AlgoController extends Controller
     // Exercice 2
     public function panier($tab)
     {
+        $result = Array(
+            'total_ht' => 0,
+            'total_discount' => 0,
+            'total_tva5' => 0,
+            'total_tva20' => 0,
+            'total_ttc' => 0,
+        );
+        foreach ($tab as $product) {
+            $tva1 = $tva2 = 0;
+            if ($product['qty'] < 3) {
+                $prixht = $result['total_ht'] += $product['price_ht']*$product['qty'];
+                if ($product['code_tva'] == 1) {
+                    $tva1 += $result['total_tva5'] += ($product['price_ht']*0.05)*$product['qty'];
+                } else {
+                    $tva2 += $result['total_tva20'] += ($product['price_ht']*0.2)*$product['qty'];
+                }
+                $result['total_ttc'] = $prixht + $tva1 + $tva2;
+            } elseif ($product['qty'] < 10){
+                $discount = ($product['price_ht']*0.05)*$product['qty'];
+                $result['total_discount'] += $discount;
+                $prixht = $result['total_ht'] += ($product['price_ht']*$product['qty'])-$discount;
+                if ($product['code_tva'] == 1) {
+                    $tva1 += $result['total_tva5'] += (($product['price_ht']*0.05)*0.05)*$product['qty'];
+                } else {
+                    $tva2 += $result['total_tva20'] += (($product['price_ht']*0.05)*0.2)*$product['qty'];
+                }
+                $result['total_ttc'] = $prixht + $tva1 +$tva2;
 
+            } else {
+                $discount = ($product['price_ht']*0.1)*$product['qty'];
+                $result['total_discount'] += $discount;
+                $prixht = $result['total_ht'] += ($product['price_ht']*$product['qty'])-$discount;
+                if ($product['code_tva'] == 1) {
+                    $tva1 += $result['total_tva5'] += (($product['price_ht']*0.1)*0.05)*$product['qty'];
+                } else {
+                    $tva2 += $result['total_tva20'] += (($product['price_ht']*0.1)*0.2)*$product['qty'];
+                }
+                $result['total_ttc'] = $prixht + $tva1 + $tva2;
 
+            }
+        }
+        return $result;
     }
 }
