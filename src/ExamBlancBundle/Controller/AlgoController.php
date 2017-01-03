23e1ef330488d05b5843fd3c61c908d6e77d781d
Kim Nguyen <knguyen.wcs@gmail.com>
2017-01-03 18:04:48 +0100

    envoi git

--- src/ExamBlancBundle/Controller/AlgoController.php
+++ src/ExamBlancBundle/Controller/AlgoController.php
@@ -13,14 +13,19 @@ class AlgoController extends Controller
     // Exercice 1
     public function number_of_char($str)
     {
-
-
+        $table_of_letter = str_split($str);
+        $minuscule = strtolower($table_of_letter);
     }
 
     // Exercice 2
     public function panier($tab)
     {
-
-
+        $ht = array_sum ($tab);
+        $ttc = $ht * 1.2;
+        if(code_tva = 1){
+            return $ht * 1.05;
+        }
+        else(code_tva = 2){
+            return $ht * 1.20;
+        }
     }
-}
