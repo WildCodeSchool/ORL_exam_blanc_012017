78a81f267b45a8b467c805ab5f20c4d01e0eba15
MkLHX <mickael.lehoux@gmail.com>
2017-01-03 14:58:43 +0100

    entities and mapping Ok

---
+++ src/TravelBundle/Entity/Hotel.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace TravelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Hotel
+ *
+ * @ORM\Table(name="hotel")
+ * @ORM\Entity(repositoryClass="TravelBundle\Repository\HotelRepository")
+ */
+class Hotel
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     */
+    private $stars;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $address;
+
+    /**
+     * @var int
+     */
+    private $zipcode;
+
+    /**
+     * @var string
+     */
+    private $city;
+
+    /**
+     * @var int
+     */
+    private $phone;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $website;
+
+    /**
+     * @var int
+     */
+    private $capacity;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStars()
+    {
+        return $this->stars;
+    }
+
+    /**
+     * @param mixed $stars
+     */
+    public function setStars($stars)
+    {
+        $this->stars = $stars;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return int
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * @param int $zipcode
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param int $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    /**
+     * @param string $website
+     */
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCapacity()
+    {
+        return $this->capacity;
+    }
+
+    /**
+     * @param int $capacity
+     */
+    public function setCapacity($capacity)
+    {
+        $this->capacity = $capacity;
+    }
+
+
+}
+
---
+++ src/TravelBundle/Entity/Room.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace TravelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use TravelBundle\Entity\Hotel;
+
+/**
+ * Room
+ *
+ * @ORM\Table(name="room")
+ * @ORM\Entity(repositoryClass="TravelBundle\Repository\RoomRepository")
+ */
+class Room
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     */
+    private $roomNumber;
+
+    /**
+     * @var int
+     */
+    private $capacity;
+
+    /**
+     * @var boolean
+     */
+    private $book;
+
+    /**
+     * @var int
+     *
+     * @ORM\OneToMany(targetEntity="Hotel", inversedBy="rooms")
+     *
+     */
+    private $hotel;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoomNumber()
+    {
+        return $this->roomNumber;
+    }
+
+    /**
+     * @param mixed $roomNumber
+     */
+    public function setRoomNumber($roomNumber)
+    {
+        $this->roomNumber = $roomNumber;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCapacity()
+    {
+        return $this->capacity;
+    }
+
+    /**
+     * @param int $capacity
+     */
+    public function setCapacity($capacity)
+    {
+        $this->capacity = $capacity;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isBook()
+    {
+        return $this->book;
+    }
+
+    /**
+     * @param boolean $book
+     */
+    public function setBook($book)
+    {
+        $this->book = $book;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHotel()
+    {
+        return $this->hotel;
+    }
+
+    /**
+     * @param int $hotel
+     */
+    public function setHotel($hotel)
+    {
+        $this->hotel = $hotel;
+    }
+
+
+
+}
+
